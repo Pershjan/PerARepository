654b1ec516b5c1b932ebee632796709a3aba1dea
Pershjan <per@getacademy.no>
2019-05-23 00:21:15 +0200

    vue++

---
+++ vue-pershjan/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+  <h1>{{title}}</h1>
+  <ninjas></ninjas>
+  </div>
+</template>
+
+<script>
+import ninjas from './ninja.vue'
+ export default {
+   components: {
+     'ninjas': ninjas
+   },
+  data() {
+    return {
+      title: 'Ninja App',
+    value: ''
+    }
+  }
+}
+
+
+
+</script>
+
+<style>
+
+</style>
